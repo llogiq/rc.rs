bcc84469f6e01fb968de7bec6a4b90496420e3ed
Jorge Aparicio <japaricious@gmail.com>
2015-05-15 13:55:53 +0000

    docs!

---
+++ implementors/core/hash/trait.Hash.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['rc'] = ["impl&lt;T: ?<a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Sized.html' title='core::marker::Sized'>Sized</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='struct' href='rc/struct.Rc.html' title='rc::Rc'>Rc</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/marker/trait.Sync.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['rc'] = ["impl&lt;T&gt; !<a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Sync.html' title='core::marker::Sync'>Sync</a> for <a class='struct' href='rc/struct.Rc.html' title='rc::Rc'>Rc</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
